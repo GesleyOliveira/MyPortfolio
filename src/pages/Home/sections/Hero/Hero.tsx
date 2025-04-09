6e2c7e0c2eaab36d31b832f7865a4871a0ba8b37
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-04-09 15:55:44 -0300

    correção de bugs e pacotes otimizados

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/system'; 
 import { Button, Typography, Container } from "@mui/material";
-import Grid from '@mui/material/Unstable_Grid2';
+import Grid from "@mui/material/Grid";
 import Avatar from "../../../../assets/images/avatar.jpg";
 import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
