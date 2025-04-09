fa3eb9281ff23f76e2ae0dcea3b14601742dc264
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-04-08 21:09:27 -0300

    atualização de estilização

---
+++ src/theme.ts
@@ -0,0 +1,40 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+let theme = createTheme({
+    palette: {
+      primary: {
+        main: '#232323',
+        light: '#3c3c3c', // opcional, mais claro que o main
+        dark: '#0c0c0c',  // opcional, mais escuro que o main
+        contrastText: '#ffffff', // texto branco para bom contraste
+      },
+      secondary: {
+        main: '#4f8e3e',
+        light: '#72b362', // opcional
+        dark: '#366528',  // opcional
+        contrastText: '#ffffff',
+      },
+    },
+    typography: {
+        fontFamily: "Helvetica Neue"
+    }
+  });
+
+  theme = responsiveFontSizes(theme);
+  
+
+/*const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+*/
+
+export default theme;
