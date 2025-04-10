10f1f7b805a397307bd2701fe5b5a2bec71e88dd
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-04-10 14:50:02 -0300

    fix: ajustando a imagem

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,15 +11,21 @@ import { AnimatedBackground } from 'components/AnimatedBackground/AnimatedBackgr
 
 
 
-const StyledHero = styled("div")(() => ({
+const StyledHero = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   height: "100vh",
   display: "flex",
   alignItems: "center",
+  [theme.breakpoints.up("xs")]: {
+    paddingTop: "100px",
+  },
+  [theme.breakpoints.up("md")]: {
+    paddingTop: "0",
+  },
 }));
 
 const StyledImg = styled("img")(({ theme }) => ({
-  width: "80%",
+  width: "75%",
   borderRadius: "50%",
   border: `1px solid ${theme.palette.primary.contrastText}`,
 }));
