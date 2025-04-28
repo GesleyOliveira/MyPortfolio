d7b37a15f0275dfee4fc01d47b99947e147b905d
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-04-27 23:13:09 -0300

    fix: "algumas correções"

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -6,7 +6,8 @@ import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
 import StyledButton from "../../../../components/StyledButton/StyledButton";
 import theme from "../../../../../src/theme";
-import { AnimatedBackground } from 'components/AnimatedBackground/AnimatedBackground';
+import { AnimatedBackground } from "@/components/AnimatedBackground/AnimatedBackground";
+
 import Typewriter from "../../../../components/Typewriter/Typewriter"
 import CV from "../../../../assets/pdfs/Open.pdf"
 
--- vite.conifg.ts
+++ vite.conifg.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Corrige o __dirname no contexto ESM
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Corrigir __dirname para ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Verifica se está em modo produção (por exemplo, ao rodar `vite build`)
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isProduction ? '/MyPortfolio/' : '/', // Base dinâmica
+  base: isProduction ? '/myPortfolio/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
